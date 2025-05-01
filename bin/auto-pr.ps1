c6110366b826f23709bf513367ac11060f3efbed
SNRainiar <49141130+SamuNatsu@users.noreply.github.com>
2025-05-01 13:17:55 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "SamuNatsu/rainiar-scoop-bucket:main"
+    [String]$upstream = "SamuNatsu/rainiar-scoop-bucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
